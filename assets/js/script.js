e7d2ddf78c64864bd57bf585f4eeeaea2600f6c9
Nathisaraujo <>
2023-10-19 08:13:54 +0000

    declared variables, created result class

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question = document.querySelector(".question-area");
+const answer = document.querySelector(".answer");
+const finish = document.querySelector(".finish");
+const result = document.querySelector(".result");
+const score = document.querySelector(".score");
